a91a19892290be55c9a770330d93fd40b28b1f6f
resulturfent <rsltrfnt@gmail.com>
2026-05-14 21:21:20 +0300

    template giydirme yapıldı

---
+++ AriTechno.Web/Controllers/AlisverisController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriTechno.Web.Controllers
+{
+    public class AlisverisController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+        public IActionResult ShopDetail()
+        {
+            return View();
+        }
+    }
+}
---
+++ AriTechno.Web/Controllers/UrunlerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriTechno.Web.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+   
+    }
+}
