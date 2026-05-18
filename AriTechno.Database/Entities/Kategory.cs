76f7202637daca953fa8c692c6f4711f0814a455
resulturfent <rsltrfnt@gmail.com>
2026-05-18 19:39:02 +0300

    Database katman için entities eklemesi

---
+++ AriTechno.Database/Entities/Kategory.cs
@@ -0,0 +1,8 @@
+﻿namespace AriTechno.Database.Entities;
+
+public class Kategory
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Aciklama { get; set; }
+}
