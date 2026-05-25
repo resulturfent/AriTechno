86b37b6923959ab2ea472d4ba13fcd2e018e1925
sumeyyeyener <sumeyyener.0@gmail.com>
2026-05-25 20:35:08 +0300

    admin panel oluşturuldu

---
+++ AriTechno.Web/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriTechno.Web.Areas.AdminPanel.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+
+        }
+    }
+}
