a6b97c4559c661a336beb392180e6d33efe63b15
resulturfent <rsltrfnt@gmail.com>
2026-06-16 21:16:32 +0300

    ürünler ve kategoriler beraber listelendi

--- AriTechno.Web/Controllers/UrunlerController.cs
+++ AriTechno.Web/Controllers/UrunlerController.cs
@@ -1,14 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AriTechno.Service.Services.Interfaces;
+using AriTechno.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AriTechno.Web.Controllers
 {
     public class UrunlerController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+
+        public UrunlerController(IProductService productService, ICategoryService categoryService)
         {
-            return View();
+            _productService = productService;
+            _categoryService = categoryService;
         }
 
-   
+        public IActionResult Index(int id = 0)
+        {
+            var productList = _productService.GetAll();
+
+            if (id > 0)
+            {
+                productList= productList.Where(p => p.CategoryId == id).ToList();
+            }
+            var categoryList = _categoryService.GetAll();
+
+            var productCategoryView = new ProductCategoryView
+            {
+                ProductList = productList,
+                CategoryList = categoryList
+            };
+            return View(productCategoryView);
+        }
+
+
     }
 }
---
+++ AriTechno.Web/Models/ProductCategoryView.cs
@@ -0,0 +1,9 @@
+﻿using AriTechno.Database.Entities;
+
+namespace AriTechno.Web.Models;
+
+public class ProductCategoryView
+{
+    public List<Product> ProductList { get; set; }
+    public List<Category> CategoryList { get; set; }
+}
